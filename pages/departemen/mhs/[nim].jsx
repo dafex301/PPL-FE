8de3ad1e8b94ad50ddcdbe14c0909db4b6e3cb24
Fahrel Gibran Alghany <fahrel@students.undip.ac.id>
2022-10-31 18:22:22 +0700

    fix error dosen/mhs

--- pages/departemen/mhs/[nim].jsx
+++ pages/departemen/mhs/[nim].jsx
@@ -1,20 +1,20 @@
-import React from 'react'
-import DetailPage from '../../../components/detail/detailPage';
+import React from "react";
+import DetailPage from "../../../components/detail/detailPage";
 import { useRouter } from "next/router";
- 
+
 function DetailMahasiswaDosen() {
-    // nilai NIM dari paramter
-    const router = useRouter();
-    const { nim } = router.query;
+  // nilai NIM dari paramter
+  const router = useRouter();
+  const { nim } = router.query;
   return (
     <>
-      <DetailPage role={"departemen"} API={`${process.env.BACKEND_API}/departemen/mahasiswa/${nim}`}  nim={nim} />
+      <DetailPage
+        role={"departemen"}
+        API={`${process.env.BACKEND_API}/departemen/mahasiswa/${nim}`}
+        nim={nim}
+      />
     </>
-  )
+  );
 }
 
 export default DetailMahasiswaDosen;
-
-
-
-
